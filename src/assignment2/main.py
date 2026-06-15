e1c02522af14d1e734133a1f8fc0f38077fb6d29
Liu. Alan (TPE) <Alan_Liu@compal.com>
2026-06-15 11:17:02 +0800

    changed app.mount directory location to ./static

--- src/assignment2/main.py
+++ src/assignment2/main.py
@@ -11,7 +11,7 @@ class Todo(BaseModel):
     resolved: int = 0
 
 app = FastAPI()
-app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount("/static", StaticFiles(directory="./static"), name="static")
 
 # use a get to read what is currently in the database
 # use a post to create a new todo
